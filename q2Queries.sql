01862c3d8b077e2b124b2e206a4180ec774e0bbd
TJKNL <39524446+TJKNL@users.noreply.github.com>
2019-03-12 18:59:15 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -2,7 +2,7 @@ SELECT C.CourseName, CR.Grade FROM StudentRegistrationsToDegrees as SRTD, Course
 SELECT 0;
 SELECT 0;
 SELECT COUNT(DISTINCT S.StudentId) FILTER(WHERE S.Gender='F') / CAST(COUNT(DISTINCT S.StudentId) AS DECIMAL) AS percentage FROM Students AS S, StudentRegistrationsToDegrees AS SRTD, Degrees AS D WHERE S.StudentId = SRTD.StudentId AND D.DegreeId = SRTD.DegreeId AND D.Dept = %1%; 
-SELECT CO.CourseId, AVG(case when CRWG.Grade >= 4 then 1 else 0 end) as percentagePassing FROM CourseOffers as CO, CourseRegistrationsWithGrade as CRWG WHERE CO.CourseOfferId = CRWG.CourseOfferId GROUP BY CO.CourseId;
+SELECT CO.CourseId, AVG(case when CRWG.Grade >= %1% then 1 else 0 end) as percentagePassing FROM CourseOffers as CO, CourseRegistrationsWithGrade as CRWG WHERE CO.CourseOfferId = CRWG.CourseOfferId GROUP BY CO.CourseId;
 SELECT 0;
 SELECT 0;
 SELECT C.CourseName, CO.Year, CO.Quartile FROM SACountPerCO, SCountPerCO, CourseOffers as CO, Courses as C WHERE SACountPerCO.CourseOfferId = CO.CourseOfferId AND C.CourseId = CO.CourseId AND SCountPerCO.CourseOfferId = CO.CourseOfferId AND FLOOR(SCountPerCO.Count / 50) > SACountPerCO.Count ORDER BY CO.CourseOfferId;
